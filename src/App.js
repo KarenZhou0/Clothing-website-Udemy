579430ae6f1b76296d15cbc4a57d1864777ab743
Karen Zhou <zz2202@nyu.edu>
2020-01-05 22:00:13 +0800

    added route for hats page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
