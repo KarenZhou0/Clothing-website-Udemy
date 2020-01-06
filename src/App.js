b0b02283bd0b6339eb1e67e3ab1c5969a8a98dc7
Karen Zhou <zz2202@nyu.edu>
2020-01-06 09:51:50 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
       </Switch>
-      </BrowserRouter>
     </div>
   );
 }
