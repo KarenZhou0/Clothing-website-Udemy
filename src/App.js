5cb7a64f74be2bd2fe3a0f911d92c35dfdb6f0be
Karen Zhou <zz2202@nyu.edu>
2020-01-06 15:04:40 +0800

    linked signin with google

--- src/App.js
+++ src/App.js
@@ -6,19 +6,41 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {auth} from './firebase/firebase.util'; 
 
+class App extends React.Component{
+  constructor(){
+    super();
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage}/>
-        <Route path='/shop' component={ShopPage}/>
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+    this.state={
+      currentUser:null
+    };
+  }
+
+  unsubcribeFromAuth = null
+
+  componentDidMount(){
+    this.unsubcribeFromAuth=auth.onAuthStateChanged(user=>{
+      this.setState({currentUser:user});
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubcribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
