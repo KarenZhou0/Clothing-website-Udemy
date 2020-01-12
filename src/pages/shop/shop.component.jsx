3a21eb209c700ce4cd571b2d3caa3ff39bbf23eb
Karen Zhou <zz2202@nyu.edu>
2020-01-12 18:58:56 -0500

    created individual collection pages for all collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -7,7 +7,7 @@ const ShopPage = ({match}) => {
     return(
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route path={`${match.path}/:collecrtionId`} component={CollectionPage}/>
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
     </div>
 )};
 
