077207b2b0570c9f0a801961da1fab7db8b4e9c3
Karen Zhou <zz2202@nyu.edu>
2020-01-13 03:32:20 -0500

    changed title and icon, integrated stripe checkout page

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_tBkVN9tMI061AG7I7nQ26V5N';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
