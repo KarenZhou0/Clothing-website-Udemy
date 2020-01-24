66b7154353e8a3436f8dd19aa778afb517db32d9
Karen Zhou <zz2202@nyu.edu>
2020-01-24 13:54:49 -0500

    a little bit more

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -24,7 +24,11 @@ class ShopPage extends React.Component {
         const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot=> {
+        // fetch('https://firestore.googleapis.com/v1/projects/crwn-db-a1857/databases/(default)/documents/collections')
+        // .then(response => response.json())
+        // .then(collections => console.log(collections))
+
+        collectionRef.get().then(snapshot=> {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({loading: false});
